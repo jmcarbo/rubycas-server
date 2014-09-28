2711a2bc71d8bbd4bb9dcd507a80e136ed3afa11
Philipp Hug <philipp@hug.cx>
2014-09-28 14:01:34 +0000

    dont show config in logs

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd /rubycas-server
 sed -i 's/REPLACE_THIS_PASSWORD/'"$LDAP_PASSWORD"'/' /etc/rubycas-server.yml
-cat /etc/rubycas-server.yml
 exec bundle exec rubycas-server -c /etc/rubycas-server.yml
 
